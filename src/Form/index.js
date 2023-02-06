2d14d26a9d2036e840997454a58cbad9ad5893e0
JoannaDraganek <98357183+JoannaDraganek@users.noreply.github.com>
2023-02-06 19:23:59 +0100

    code formatted

--- src/Form/index.js
+++ src/Form/index.js
@@ -2,8 +2,8 @@ import "./style.css";
 
 const Form = () => (
     <form className="form">
-                <input className="form__input" placeholder="Co jest do zrobienia?" autofocus/>
-                <button className="form__button">Dodaj zadanie</button>
+        <input className="form__input" placeholder="Co jest do zrobienia?" autofocus/>
+        <button className="form__button">Dodaj zadanie</button>
     </form>
 );
 
--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,17 +1,29 @@
 import "./style.css";
 
-const Tasks = ({tasks, hideDoneTasks}) => (
-    <ul className="tasks">
-      
-      {tasks.map(task => (<li key={task.id} className={`tasks__item ${task.done && hideDoneTasks ? "task__item--hidden" : ""}`}
-         >
-          <button className="task__button task__buttonToggleDone">{task.done ? "✓" : " "}</button>
-          <li key={task.id} className={`tasks__content ${task.done && hideDoneTasks ? "task__item--hidden" : ""}`}>
-            <span className={`tasks__content {task.done ? "task--done" : ""}`}>{task.content}</span>
-          </li> 
-          <button className="task__button task__binButton">️🗑️</button>
-        </li>))}
-    </ul>
+const Tasks = ({ tasks, hideDoneTasks }) => (
+  <ul className="tasks">
+    {tasks.map((task) => (
+      <li key={task.id} className={`tasks__item ${
+          task.done && hideDoneTasks ? "tasks__item--hidden" : ""
+        }`}
+      >
+        <button className="task__button task__buttonToggleDone">
+          {task.done ? "✓" : " "}
+        </button>
+        <li
+          key={task.id}
+          className={`tasks__content ${
+            task.done && hideDoneTasks ? "tasks__item--hidden" : ""
+          }`}
+        >
+          <span className={`tasks__content {task.done ? "task--done" : ""}`}>
+            {task.content}
+          </span>
+        </li>
+        <button className="task__button task__binButton">️🗑️</button>
+      </li>
+    ))}
+  </ul>
 );
 
 export default Tasks;
